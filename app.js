47cc9056bdc93d515706a612ff9c1fa69d5a234f
ElDav1d <dvivoes@yahoo.es>
2023-02-07 17:22:56 +0100

    Add routes

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const express = require("express")
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get("/",(req, res) => {
-    res.send("Hello World!")
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/home", (req, res) => {
+  res.send("Home");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About");
+});
+
+app.get("/works", (req, res) => {
+  res.send("Works");
+});
 
 app.listen(port, () => {
-    console.log("Whatever here.")
-})
+  console.log("Whatever here.");
+});
